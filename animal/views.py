51dfbeb6849f44cf1df9b2eb885018d1557dc10a
Josguther1234 <josguther@mesoamericana.edu.gt>
2019-11-06 19:00:10 -0600

    se modifico el archivo views para vincular con los formularios

--- animal/views.py
+++ animal/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import AnimalForm
+from animal.models import Animal, Encargado
 
-# Create your views here.
+def Horario_Huevo(request):
+    if request.method == "POST":
+        formulario = HorarioCuidadoForm(request.POST)
+        if formulario.is_valid():
+            animal = Animal.objects.create(nombre=formulario.cleaned_data['nombre'], anio = formulario.cleaned_data['anio'])
+            for encargado_id in request.POST.getlist('encargado'):
+                horario = Actuacion(animal_id=animal_id, encargado_id = encargado.id)
+                horario.save()
+            messages.add_message(request, messages.SUCCESS, 'Horario Guardado con Exito')
+    else:
+        formulario = HorarioCuidadoForm()
+    return render(request, 'horario/horario_editar.html', {'formulario': formulario})
--- url.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-from django.conf.urls import url
-
-
-url(r'', include('animal.urls')),
